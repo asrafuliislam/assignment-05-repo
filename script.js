bac8b2e61b30430c1616845eabfd6a40c5b25e8a
asrafuliislam <asrafulshaheen8@gmail.com>
2025-08-29 22:37:06 +0600

    all section add

--- script.js
+++ script.js
@@ -1,16 +1,80 @@
+const cards = document.getElementsByClassName("Call-btn");
+const card_id = document.getElementById("Coin");
 
+let value = parseInt(card_id.textContent);
+let save_data = []; 
 
+for (let card of cards) {
+  card.addEventListener('click', function(e) {
+    e.preventDefault();
 
-document.getElementById('Call-btn').addEventListener('click', function(e){
-    e.preventDefault()
-    const coins = parseInt(document.getElementById('Coin').innerText)
-
-    if(coins>0){
-        alert('calling');
-        const newCoin = coins-20;
-        document.getElementById('Coin').innerText = newCoin;
+    if (value < 20) {
+      alert("❌ You don’t have enough coins; making a call requires at least 20 coins.");
+      return;
     }
-    else{
-        alert('you have to recharge')
-    }
-})
+
+    let parentCard = card.closest(".parent");
+    let message_value = parentCard.querySelector(".message-value").textContent;
+    let message_title = parentCard.querySelector(".message-title").textContent;
+    let message_number = parentCard.querySelector(".message-number").textContent;
+
+    value -= 20;
+    card_id.textContent = value;
+
+    alert("📞 " + message_value + " " + message_number);
+
+    const data = {
+      name: message_title,
+      amount: message_number,
+      date: new Date().toLocaleTimeString()
+    };
+
+    save_data.push(data);
+
+    const transactionPart = document.getElementById("history-add");
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <div class="bg-[#f4f5f7] rounded-xl p-3 flex justify-between items-center mb-3">
+        <div>
+          <h1>${data.name}</h1>
+          <div class="flex justify-between items-center">
+            <span>${data.amount}</span>
+            <span style="margin-left: 50px;">${data.date}</span>
+          </div>
+        </div>
+      </div>
+    `;
+    transactionPart.prepend(div);
+  });
+}
+
+// remove history
+const clear_button = document.getElementById("clear-button");
+clear_button.addEventListener("click", function () {
+  save_data = [];
+  document.getElementById("history-add").innerHTML = "";
+});
+
+// heart
+const buttons = document.getElementsByClassName('heart-btn');
+for (let btn of buttons) {
+  btn.addEventListener('click', function () {
+    const hearts = parseInt(document.getElementById('heart').innerText);
+    document.getElementById('heart').innerText = hearts + 1;
+  });
+}
+
+// copy section
+const copyNumbers = document.getElementsByClassName('copyNumber');
+const numbers = document.getElementsByClassName('number');
+
+for (let i = 0; i < copyNumbers.length; i++) {
+  copyNumbers[i].addEventListener('click', function () {
+    let text = numbers[i].innerText; 
+    navigator.clipboard.writeText(text);
+    alert("Number has been copied: " + text);
+
+    const copyCount = parseInt(document.getElementById('copy-count').innerText);
+    document.getElementById('copy-count').innerText = copyCount + 1;
+  });
+}
